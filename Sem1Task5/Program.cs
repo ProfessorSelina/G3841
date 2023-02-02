4a8447aa1635c01c15b5c06f7ccb0608897a13c9
Selina <mrs.erofa@mail.ru>
2023-02-02 23:49:27 +0300

    5 задача дз

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая на 
+// вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// Считываем данные с консоли 1 число
+    Console.WriteLine ("Введите число 1");
+  string? inputNum1 = Console.ReadLine();
+
+// Считываем данные с консоли 2 число
+    Console.WriteLine ("Введите число 2");
+  string? inputNum2 = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if (inputNum1 != null && inputNum2 != null)
+{
+    int number1=int.Parse(inputNum1);
+    int number2=int.Parse(inputNum2);
+    if (number1>number2)
+    {
+        Console.Write ("Число ");
+        Console.Write (number1);
+        Console.Write ("больше");
+        Console.Write (number2);
+    }
+    else
+    {
+        Console.Write (" Число ");
+
+
+        Console.Write (number2);
+
+        
+        Console.Write (" больше числа ");
+
+
+
+        Console.Write (number1);
+
+
+    }
+}
