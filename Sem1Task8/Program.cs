dbe48ae5a7e457afa8579fd0021a7c86ba58ae58
Selina <mrs.erofa@mail.ru>
2023-02-03 11:31:20 +0300

    8 задача

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 8: Напишите программу, которая 
+// на вход принимает число (N), а на выходе показывает 
+// все чётные числа от 1 до N.
+
+
+// Считываем данные с консоли 
+    Console.WriteLine ("Введите целое число");
+    string? inputNum = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if (inputNum != null);
+{
+     int number = int.Parse(inputNum);
+
+     // вводим пустую строку
+    string outNumLine = string.Empty;
+
+    // вводим счетчик начиная с 2
+    int count = 2;
+    // если число четное:
+    if (number % 2 == 0)
+    {
+        while (count < number)
+        {
+            outNumLine = outNumLine + count + ",";
+            count = count + 2;
+        }
+
+        // Вывод результата в консоль
+        Console.WriteLine(outNumLine + number);
+    }
+
+    //если число не четное:
+    else
+    {
+        // если число равно 1
+        if (number == 1)
+        {
+            Console.WriteLine("Введите число больше 1");
+        }
+        else
+        {
+            while (count < number - 1)
+            {
+                outNumLine = outNumLine + count + ",";
+                count = count + 2;
+            }
+            // Выводим результат в консоль
+            Console.WriteLine(outNumLine + (number - 1));
+        }
+
+    }
+
+}
