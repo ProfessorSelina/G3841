0826f7a0441226a96911001bc1f6c55ea11f1a65
Selina <mrs.erofa@mail.ru>
2023-02-07 12:42:58 +0300

    10 задача

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая 
+// принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+Console.WriteLine ("Введите 3-х значное число:");
+try {
+	int Value = Convert.ToInt32(Console.ReadLine());
+	int Length = Value.ToString().Length;
+	if (Length == 3) 
+    {
+		int result = ( Value/10 ) % 10;
+		Console.WriteLine($"Вторая цифра {result}");
+	} 
+    else 
+    {
+		Console.WriteLine("Не трехзначное число");
+	}
+} catch (System.Threading.ThreadAbortException) {
+	System.Threading.Thread.ResetAbort();
+} catch (Exception e) {
+	Console.WriteLine(e.Message);
+}
