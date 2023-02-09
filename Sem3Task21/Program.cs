4359667dd68a1aa8107d76619c2bbd0fdb571855
Selina <mrs.erofa@mail.ru>
2023-02-09 12:50:14 +0300

    21 задача

--- Sem3Task21/Program.cs
+++ Sem3Task21/Program.cs
@@ -19,7 +19,7 @@ int Coordinate(string coorName, string pointName)
 double Decision(double x1, double x2, 
                 double y1, double y2, 
                 double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+return Math.Sqrt(Math.Pow((x2-x1), 2) + 
                    Math.Pow((y2-y1), 2) + 
                    Math.Pow((z2-z1), 2));
 }
