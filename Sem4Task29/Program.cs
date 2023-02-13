b678164275b35205f0d8e59fe7cd8c879caa9d7b
Selina <mrs.erofa@mail.ru>
2023-02-13 13:37:40 +0300

    29 задача

---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+            Console.WriteLine("Элементы массива:");
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+        }
+    }
+}
